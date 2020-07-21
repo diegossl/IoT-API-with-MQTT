fb13d8fd384bac5adef4f290f0c706f6b28dc00f
diegossl <diegossl94@gmail.com>
2020-07-20 23:39:02 -0300

    Connection failed

--- api/Gateway.ts
+++ api/Gateway.ts
@@ -7,10 +7,23 @@ export default {
   },
 
   registerSubscribed (request: Request, response: Response) {
-
+    try {
+      const topic: string | string[] = request.body.topic
+      console.log(topic)
+      MqttService.addSubscriber(topic)
+      return response.status(200).send('Subscriber successfully registered')
+    } catch (error) {
+      return response.status(500).send('Internal system error')
+    }
   },
 
   removeSubscribed (request: Request, response: Response) {
-
+    try {
+      const topic: string | string[] = request.body.topic
+      MqttService.removeSubscriber(topic)
+      return response.status(200).send('Subscriber removed successfully')
+    } catch (error) {
+      return response.status(500).send('Internal system error')
+    }
   }
 }
--- config/mqtt.ts
+++ config/mqtt.ts
@@ -1,18 +1,35 @@
 import mqtt, { MqttClient } from 'mqtt'
 
-export default {
-  connect (): MqttClient {
-    const client: MqttClient = mqtt.connect(process.env.MQTT_TEST)
+class Mqtt {
+  private static instance: Mqtt
 
-    client.on('error', (err) => {
-      console.log(err)
-      client.end()
-    })
+  private constructor () { }
 
-    client.on('connect', () => {
-      console.log('MQTT client connected')
-    })
+  public static getInstance (): Mqtt {
+    if (!this.instance) {
+      this.instance = new Mqtt()
+    }
+    return this.instance
+  }
 
-    return client
+  public connect (): MqttClient | undefined {
+    const urlConnection: string | undefined = process.env.MQTT_TEST
+    console.log(urlConnection)
+    if (urlConnection) {
+      const client: MqttClient = mqtt.connect(urlConnection)
+
+      client.on('error', (err) => {
+        console.log(err)
+        client.end()
+      })
+
+      client.on('connect', () => {
+        console.log('MQTT client connected')
+      })
+      return client
+    }
+    return undefined
   }
 }
+
+export default Mqtt.getInstance()
